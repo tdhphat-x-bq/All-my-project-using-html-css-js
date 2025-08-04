2c226c692a1e0afda4ea1b69fb0d46496b7d9637
phattdh2007@gmail.com <tdhphat337>
2025-08-04 14:34:17 +0700

    ppp

--- MyEvolution/app.js
+++ MyEvolution/app.js
@@ -1,23 +1,52 @@
-const box = document.querySelector(".main");
-let imgs, vel = -190, l = 0, step = 1;
+const content = document.querySelector(".content");
+let l = 0, deg = -360, time = 0, run = null, run1 = null, step = -290;
+let tl = gsap.timeline(), tl1 = gsap.timeline();
 
 for (let i = 0; i < 17; i++) {
+    let s;
+    const item = document.createElement("div");
     const img = document.createElement("img");
+    const text = document.createElement("p");
+
     img.src = `assets/p1 (${i}).jpg`;
-    box.append(img);
-    box.lastChild.style.left = `${l}px`;
-    l += 190;
+
+    if (i > -1 && i < 4) s = "Adult";
+    if (i > 3 && i < 10) s = "Child";
+    if (i > 9) s = "My Sister";
+    if (i == 16) s = "Sigma ☠️"; 
+
+    text.innerText = s;
+    item.append(img); item.append(text);
+    item.style.left = `${l}px`;
+    content.append(item);
+    l += 290;
 }
-imgs = box.children;
-let run = setInterval(() => {
-    box.style.transform = `translateX(${vel}px)`;
-    vel -= 190;
-    setTimeout(() => {
-        const img = imgs[0].cloneNode(true);
-        imgs[0].remove();
-        box.append(img);
-        box.lastChild.style.left = `${l}px`;
-        imgs = box.children;
-        l += 190;
-    }, 1000);
+
+run = setInterval(() => {
+    tl.to(".circle", {
+        duration: 1.5,
+        ease: "linear",
+        rotate: deg,
+    })
+    deg -= 360; time = 2000;
+}, time)
+
+run = setInterval(() => {
+    tl1.to(".content > div", {
+        duration: 1.5,
+        ease: "linear",
+        x: step,
+    });
+    step -= 290;
 }, 2000);
+
+setInterval(() => {
+    let pos = content.children[0].getBoundingClientRect();
+    if (Math.floor(pos.left) == -89) {
+        const item = content.children[0].cloneNode(true);
+        content.children[0].remove();
+        item.style.left = `${l}px`;
+        content.append(item);
+        l += 290;
+    }
+}, 5); 
