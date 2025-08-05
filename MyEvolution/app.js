e81e4663a64310f94ecb9a2f714f8f87f8e1a91c
phattdh2007@gmail.com <tdhphat337>
2025-08-05 08:52:48 +0700

    ppp

--- MyEvolution/app.js
+++ MyEvolution/app.js
@@ -2,7 +2,7 @@ const content = document.querySelector(".content");
 let l = 0, deg = -360, time = 0, run = null, run1 = null, step = -290;
 let tl = gsap.timeline(), tl1 = gsap.timeline();
 
-for (let i = 0; i < 17; i++) {
+for (let i = 0; i < 21; i++) {
     let s;
     const item = document.createElement("div");
     const img = document.createElement("img");
@@ -13,7 +13,6 @@ for (let i = 0; i < 17; i++) {
     if (i > -1 && i < 4) s = "Adult";
     if (i > 3 && i < 10) s = "Child";
     if (i > 9) s = "My Sister";
-    if (i == 16) s = "Sigma ☠️"; 
 
     text.innerText = s;
     item.append(img); item.append(text);
